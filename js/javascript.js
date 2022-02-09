dfa191167b389e3befbc79645bfcb8a1e77f0645
MasterTebogo <98981101+MasterTebogo@users.noreply.github.com>
2022-02-09 12:21:20 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello, World! again this is javascript on a console.log")
+console.log("Hello, World! again this is javascript on a console.log");
+
+
+let a = 10;
+let bodmas = (4 + 6 - 9) / 779 * a
+console.log(bodmas)
+console.log(b)
+
+
