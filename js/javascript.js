21943a2d17757f922ddc41246e21b5ac46ecb247
MasterTebogo <98981101+MasterTebogo@users.noreply.github.com>
2022-02-09 12:32:39 +0200

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -11,3 +11,19 @@ function favoriteAnimal(animal) {
 }
 
 favoriteAnimal('Sheep')
+
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
+
+
+
+
+
